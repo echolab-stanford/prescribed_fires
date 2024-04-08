d2ee57d4ab1b57d5da6188748e31be64cc5de9fe
Ivan Higuera-Mendieta <bennytheball@fastmail.com>
2024-04-08 15:00:26 -0700

    Organize the house a bit and consolidate utils
    This commit creates some modules to clean the house a bit and to keep
    things separated and cleaner. Now, we still need to add a builder module
    that takes care of the dataset creation rather than having things in
    Jupyter notebooks. Classes can be messy, but also functions are annoying
    as we need like 10 functions to build things.
    
    Classes might be heavier?
    
    TODO:
     - Add estimate module with all the CBPS code

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import rasterio
+import xarray as xr
 
 
 def expand_grid(dict_vars):
@@ -23,3 +25,60 @@ def expand_grid(dict_vars):
     data_dict = {var: m.flatten() for var, m in zip(dict_vars.keys(), mesh)}
 
     return pd.DataFrame(data_dict)
+
+
+def transform_array_to_xarray(data, transform, extra_dims=None):
+    """Transform numpy array to xarray using an affine transform (from rasterio)
+
+    This function takes a numpy array with a transform affine matrix and return
+    an xarray with latitude and longitude coordinates and extra dimensions if
+    passed.
+
+    TODO:
+    ----
+    CRS is not passed as an option, but that would be a nice addition!
+
+    Parameters
+    ----------
+    data : np.array
+        Array with data
+    transform : affine.Affine
+        Affine transform matrix
+    extra_dims : list, optional
+        List with extra dimensions to add to the xarray
+
+    Returns
+    -------
+        An xarray with the data and coordinates
+    """
+
+    height = data.shape[0]
+    width = data.shape[1]
+
+    cols, rows = np.meshgrid(np.arange(width), np.arange(height))
+    xs, ys = rasterio.transform.xy(transform, rows, cols)
+
+    # Concat into arrays
+    lons = np.array(xs)
+    lats = np.array(ys)
+
+    # Create xarray
+    coords = {"lat": lats[:, 0], "lon": lons[0, :]}
+
+    if extra_dims:
+        coords.update(extra_dims)
+
+        # Take care of array dimensions. Dimensions should be at minimum 2D
+        # by construction. If extra dims are passed, we need to add a new axis
+        # to the data array if the data is not there.
+        # For example:
+        # data.shape = (10, 10) # lon and lat
+        # data.shape = (10, 10, 1) # lon, lat and time.
+        # We need to pass an array with the last shape if we have extra dims.
+
+        extra_dims_len = len(extra_dims)
+
+        if len(data.shape) == 2:
+            data = data[(...,) + (np.newaxis,) * extra_dims_len]
+
+    return xr.DataArray(data, coords=coords, dims=list(coords.keys()))
